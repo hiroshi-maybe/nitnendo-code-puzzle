44ea9784c86742f3b9dcc111af12b29761d9ee4b
Hiroshi Kori <kori39@gmail.com>
2012-09-21 04:46:18 -0700

    Added java version

---
+++ question_1/FirstCode.java
@@ -0,0 +1,18 @@
+import java.math.BigDecimal;
+
+public class FirstCode {
+  static final BigDecimal divider = new BigDecimal(3569);
+  static final BigDecimal adder = new BigDecimal(1);
+  public static void main(String[] args) {
+    BigDecimal i = new BigDecimal(0);
+    while(true) {
+      BigDecimal j=i.pow(17);
+      j=j.remainder(divider);
+      if (j.intValue()==915) {
+	System.out.println(i);
+	break;
+      }
+      i=i.add(adder);
+    }
+  }
+}
