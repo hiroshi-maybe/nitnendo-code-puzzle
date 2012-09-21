35e33b84c02e17f658b08e340c1af6ed8e743490
Hiroshi Kori <kori39@gmail.com>
2012-09-21 04:21:48 -0700

    Integrated a structure to one function

--- question_4/second_code.py
+++ question_4/second_code.py
@@ -1,16 +1,11 @@
 # Nintendo Code Puzzle Question 3
 # http://cp1.nintendo.co.jp/moon
-#
 
 class Bars(list):
     
     def __init__(self, str):
         super(Bars, self).__init__(list(str))
 
-    def prev_i(self, i):
-        return len(self)+i-1 if i-1<0 else i-1
-    def next_i(self, i):
-        return i-len(self)+1 if i>len(self)-2 else i+1
     def xor(self, left, right):
         return bool(left)!=bool(right)
 
@@ -21,29 +16,29 @@ class Bars(list):
     def bothside_rule(self, prev_v, next_v, ins, i):
         if ( (prev_v=='i' and next_v=='I') or (prev_v=='I' and next_v=='i') ):
             self[i]=ins
+        
+    def apply_complex_rule(self, prev_v, next_v, i, oneside_replace_char, bothside_replace_char):
+        self.oneside_rule(prev_v, next_v, oneside_replace_char, i)
+        self.bothside_rule(prev_v, next_v, bothside_replace_char, i)
 
     def next(self):
         prev_list=self[:]
         for i in range(len(prev_list)):
             self[i]=' '
-            prev_v=prev_list[self.prev_i(i)]
-            next_v=prev_list[self.next_i(i)]
+            prev_v=prev_list[i-1]
+            next_v=prev_list[(i+1)%len(prev_list)]
             if prev_list[i]=='i':
                 self[i]='T'
-                self.oneside_rule(prev_v, next_v, 'I', i)
-                self.bothside_rule(prev_v, next_v, 'T', i)
+                self.apply_complex_rule(prev_v, next_v, i, 'I', 'T')
             elif prev_list[i]=='T':
                 self[i]='i'
-                self.oneside_rule(prev_v, next_v, ' ', i)
-                self.bothside_rule(prev_v, next_v, 'i', i)
+                self.apply_complex_rule(prev_v, next_v, i, ' ', 'i')
             elif prev_list[i]=='I':
                 self[i]='I'
-                self.oneside_rule(prev_v, next_v, 'T', i)
-                self.bothside_rule(prev_v, next_v, 'I', i)
+                self.apply_complex_rule(prev_v, next_v, i, 'T', 'I')
             else:
                 # case " "
-                self.oneside_rule(prev_v, next_v, 'i', i)
-                self.bothside_rule(prev_v, next_v, ' ', i)
+                self.apply_complex_rule(prev_v, next_v, i, 'i', ' ')
         return self
 
     def __str__(self):
